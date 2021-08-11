5e1d8009f0d7ce7b76bd31960b6ad6f0956470b2
DrewS <1251956930@qq.com>
2021-08-11 20:21:03 +0800

    updated

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #Print verbose information or not
-VERBOSE_MODE = True
+VERBOSE_MODE = False
 
 HEADERS = {
    "user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
